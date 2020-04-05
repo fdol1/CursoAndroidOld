b9d339a064bbafa472791720b409c099040eeaf4
Fernando_Pineda <l1_fdo@hotmail.com>
2020-04-04 23:04:48 -0500

    cambio entre fragmentos

---
+++ app/src/main/java/com/example/lfpu/cursoandroid/FragmentModoNoche.java
@@ -0,0 +1,28 @@
+package com.example.lfpu.cursoandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentModoNoche extends Fragment {
+
+    public FragmentModoNoche() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modo_noche, container, false);
+    }
+}
--- app/src/main/java/com/example/lfpu/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/lfpu/cursoandroid/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lfpu.cursoandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
+
+    Boolean modoDia = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Carga del fragmento por defecto
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contenedor, new FragmentModoDia())
+                .commit();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                Fragment fragment = null;
+                if(modoDia){
+                    fragment = new FragmentModoNoche();
+                }else{
+                    fragment = new FragmentModoDia();
+                }
+
+                modoDia = !modoDia;
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contenedor, fragment)
+                        .commit();
+            }
+        });
     }
+
 }
