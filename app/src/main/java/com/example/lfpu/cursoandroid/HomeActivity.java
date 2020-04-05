dea1f6de009098805fffda5e266c01a3d2592275
Fernando_Pineda <l1_fdo@hotmail.com>
2020-04-04 22:21:26 -0500

    lista simple funcionando

--- app/src/main/java/com/example/lfpu/cursoandroid/HomeActivity.java
+++ app/src/main/java/com/example/lfpu/cursoandroid/HomeActivity.java
@@ -3,11 +3,17 @@ package com.example.lfpu.cursoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    TextView txtUsiario;
+    ListView lstLista;
+    List<String> lstAndroidVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,20 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         Bundle bundle = getIntent().getExtras();
+        setTitle(bundle.getString("email"));
 
-        txtUsiario = findViewById(R.id.txtUsuario);
-        txtUsiario.setText(bundle.getString("email"));
+        lstLista = findViewById(R.id.lst_lista);
+
+        lstAndroidVersion = new ArrayList<>();
+        lstAndroidVersion.add("hola");
+        lstAndroidVersion.add("mundo");
+        lstAndroidVersion.add("android");
+        lstAndroidVersion.add("java");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(
+                this,android.R.layout.simple_list_item_1,lstAndroidVersion);
+
+        lstLista.setAdapter(arrayAdapter);
+
     }
 }
