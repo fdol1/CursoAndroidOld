7bbf55e934edcc033afc601a3a19ad8cfed6b7c3
fernando pineda urrego <l1_fdo@hotmail.com>
2022-08-20 12:42:49 -0500

    Se soluciona error con dependecia de google

--- app/src/main/java/com/example/lfpu/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/lfpu/cursoandroid/MainActivity.java
@@ -2,14 +2,14 @@ package com.example.lfpu.cursoandroid;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
